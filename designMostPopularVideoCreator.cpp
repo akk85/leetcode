98b6a2055631b68d56628a4b95f37fab7f555304
Tony Kariuki <dev@tonykariuki.com>
2026-02-17 23:05:04 -0800

    design problems

---
+++ designMostPopularVideoCreator.cpp
@@ -0,0 +1,91 @@
+#include "common.hpp"
+
+/*
+You are given two string arrays creators and ids, and an integer array views, all of length n. The ith video on a platform was created by creators[i], has an id of ids[i], and has views[i] views.
+
+The popularity of a creator is the sum of the number of views on all of the creator's videos. Find the creator with the highest popularity and the id of their most viewed video.
+
+If multiple creators have the highest popularity, find all of them.
+If multiple videos have the highest view count for a creator, find the lexicographically smallest id.
+Note: It is possible for different videos to have the same id, meaning that ids do not uniquely identify a video. For example, two videos with the same ID are considered as distinct videos with their own viewcount.
+
+Return a 2D array of strings answer where answer[i] = [creatorsi, idi] means that creatorsi has the highest popularity and idi is the id of their most popular video. The answer can be returned in any order.
+
+
+Thought process:
+    use a map mapping each creator to their info totalviews, bestvideoviews, bestvideoid
+*/
+
+class Solution {
+private:
+    struct creatorInfo{
+        long long totalViews = 0;
+        int       maxVideoViews = 0;
+        string    bestVideoID;
+    };
+    unordered_map<string, creatorInfo> creatorsMap;
+public:
+    vector<vector<string>> mostPopularCreator(vector<string>& creators, vector<string>& ids, vector<int>& views) {
+        int n = creators.size();
+        for (int i = 0; i < n; i++){
+            string creator = creators[i];
+            string videoID = ids[i];
+            int videoViews = views[i];
+
+            addCreator(creator, videoID, videoViews);
+        }
+        vector<vector<string>> answer = getPopularCreators();
+        return answer;
+    }
+
+    void addCreator(string& creator, string& videoID, int videoViews){
+        creatorInfo& info = creatorsMap[creator];
+
+        info.totalViews += videoViews;
+
+        if (info.bestVideoID.empty() || videoViews > info.maxVideoViews){ // case when starting off list 
+            info.maxVideoViews = videoViews;
+            info.bestVideoID = videoID;
+        }
+
+        else if (videoViews == info.maxVideoViews && videoID < info.bestVideoID){
+            info.bestVideoID = videoID;
+        }
+    }
+
+    vector<vector<string>> getPopularCreators(){
+        long long highestVideoViews = 0;
+        for (const auto& [creator, info] : creatorsMap){
+            if (info.totalViews > highestVideoViews){
+                highestVideoViews = info.totalViews;
+            }
+        }
+        vector<vector<string>> result;
+        for (const auto& [creator, info] : creatorsMap){
+            if (info.totalViews == highestVideoViews){
+                result.push_back({creator, info.bestVideoID});
+            }
+        }
+
+        return result;
+    }
+};
+
+void printResult(vector<vector<string>>& ans){
+    for (auto& v : ans) {
+        cout << "[" << v[0] << "," << v[1] << "] ";
+    }
+    cout << endl;
+}
+
+int main() {
+    Solution sol;
+
+    vector<string> creators = {"alice","bob","alice","chris"};
+    vector<string> ids      = {"one", "two", "three","four"};
+    vector<int> views       = {5,10,5,4};
+
+    vector<vector<string>> answer = sol.mostPopularCreator(creators, ids, views);
+
+    printResult(answer); // [["alice","one"],["bob","two"]]
+}
---
+++ search2DMatrix.cpp
@@ -0,0 +1,119 @@
+#include "common.hpp"
+
+/*
+You are given an m x n 2-D integer array matrix and an integer target.
+
+Each row in matrix is sorted in non-decreasing order.
+The first integer of every row is greater than the last integer of the previous row.
+Return true if target exists within matrix or false otherwise.
+
+Can you write a solution that runs in O(log(m * n)) time?
+
+Intution
+    1. Find the row where this value might be as using binary search
+    2. Find if this value is in this row by doing binary search again on row
+    3. return true if found else false;
+
+pseudo code
+    int findRow(grid, target)
+        int l = 0
+        int r = rows - 1
+        while l < r
+            int m = (l + r) / 2
+            if target > row last element
+            l = m + 1
+            elif if target < row first element
+            r = m - 1
+            else    
+                return m
+
+        return -1
+    
+    bool searchMatrix(grid, target)
+        int row = findRow(grid, target);
+        if (row == -1) return false;
+
+        bool result = binarySearchRow(grid, row, target)
+        return result
+
+    bool binarySearchRow(grid, row, target)
+        int l = 0
+        int r = cols - 1
+        while l < r
+            m = (l + r) / 2
+            if target > grid[row][m]
+                l = m + 1
+            elif target < grid[row][m]
+                r = m - 1
+            else 
+                return true
+
+        return false;
+*/
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = findRow(matrix, target);
+        if (row == -1) return false;
+
+        return binarySearchRow(matrix, row, target);
+    }
+
+private:
+    int findRow(vector<vector<int>>& matrix, int target){
+        int rows = matrix.size(); int cols = matrix[0].size();
+        int l = 0;
+        int r = rows - 1;
+
+        while (l <= r){
+            int m = (l + r) / 2;
+            int firstElement = matrix[m][0];
+            int lastElement  = matrix[m][cols - 1];
+            if (target > lastElement){
+                l = m + 1;
+            }
+            else if (target < firstElement){
+                r = m - 1;
+            }
+            else {
+                return m;
+            }
+        }
+        return -1;
+    }
+
+    bool binarySearchRow(vector<vector<int>>& matrix, int row, int target){
+        int cols = matrix[0].size();
+        int l = 0;
+        int r = cols - 1;
+
+        while (l <= r){
+            int m = (l + r) / 2;
+
+            if (target > matrix[row][m]){
+                l = m + 1;
+            }
+            else if (target < matrix[row][m]){
+                r = m - 1;
+            } 
+            else {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+
+int main(){
+    Solution sol;
+
+    vector<vector<int>> matrix = {
+        {1,  2,   4,  8},
+        {10, 11, 12, 13},
+        {14, 20, 30, 40}
+    };
+
+    cout << boolalpha << sol.searchMatrix(matrix, 10) << endl;
+}
